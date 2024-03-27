28b0981a12fa9bbed4ef489b257dfc79a97c8bae
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-03-27 14:01:55 +0100

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -5,20 +5,19 @@ import {
 } from "../services/contactsServices.js";
 
 
-export const getAllContacts = async (req, res) => {
+export const getAllContacts = async (req, res, next) => {
     try {
         const result = await listContacts();
         res.json(result);
     } catch (error) {
-        res.status(500).json({
-            message: "Server error"
-        })
+
+        next(error)
     }
 
 };
 
 
-export const getOneContact = async (req, res) => {
+export const getOneContact = async (req, res, next) => {
     try {
         const { id } = req.params;
         const result = await getContactById(id);
@@ -29,10 +28,8 @@ export const getOneContact = async (req, res) => {
         res.json(result);
 
     } catch (error) {
+        next(error)
 
-        res.status(500).json({
-            message: "Server error"
-        })
     }
 };
 
